34499a29bd44db4cd126094b4a2cd34ffb6d8a74
samwfrancis <123979189+samwfrancis@users.noreply.github.com>
2025-02-27 16:48:46 +0000

    update ball pickup script

--- Dissertation/Assets/Scripts/BallPickup.cs
+++ Dissertation/Assets/Scripts/BallPickup.cs
@@ -7,7 +7,9 @@ public class BallPickup : MonoBehaviour
     [SerializeField] private GameObject ball;
     [SerializeField] private GameObject player;
 
-    public bool ballIsChild;
+    public bool ballIsChild = false;
+
+    public float distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,18 @@ public class BallPickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector3.Distance(player.transform.position, ball.transform.position); 
+
+        if (Input.GetKeyDown(KeyCode.E))
+            if(ballIsChild)
+            {
+                ball.transform.SetParent(null);
+                ballIsChild = false;
+            }
+            else if (distance < 2){
+                ball.transform.SetParent(ball.transform);
+                ballIsChild = true;
+            }
+           
     }
 }
