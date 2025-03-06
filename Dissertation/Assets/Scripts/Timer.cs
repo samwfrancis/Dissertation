4adff1e20b06e34e403d824b76836d977253ed83
samwfrancis <123979189+samwfrancis@users.noreply.github.com>
2025-03-06 12:19:04 +0000

    added goals to the puzzles

--- Dissertation/Assets/Scripts/Timer.cs
+++ Dissertation/Assets/Scripts/Timer.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     public float secondsElasped;
+
+    public float windTime;
+    private float magnetTime;
+    private float boulderTime;
+
+    public GameObject player;
+    public GameObject shipPart1;
+    public GameObject shipPart2;
+    public GameObject shipPart3;
+
+    public float windDistance;
+    public float magnetDistance;
+    public float boulderDistance;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +28,28 @@ public class Timer : MonoBehaviour
     void Update()
     {
         secondsElasped = Time.time;
+
+        windDistance = Vector3.Distance(shipPart3.transform.position, player.transform.position);
+        magnetDistance = Vector3.Distance(shipPart2.transform.position, player.transform.position);
+        boulderDistance = Vector3.Distance(shipPart1.transform.position, player.transform.position);
+
+        if(windDistance < 3){
+            if (Input.GetKeyDown(KeyCode.E)){
+                windTime = secondsElasped;
+            }
+        }
+
     }
+
+    public float GetWindTime(){
+        return windTime;
+    }
+
+    public void SetWindTime(float inputTime){
+        windTime = inputTime;
+    } 
+
+
+
+
 }
